06e153a5ca38647b0d98b1c5c8819278ccea5d44
LionOrion0507 <alexleonc12@outlook.com>
2025-03-02 19:33:15 -0600

    add CORS to flask

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import os
 from routes.id_routes import GetAllRecords, ParseIDPhoto, ParseEdoCtaPDF
 from routes.ai_routes import ChatWithLlama, InitialGreeting, UploadFile, InitialGreetingV2
 from config.database import DatabaseConnection
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  # Allow all domains
     api = Api(app)
     
     # Initialize DB connection
